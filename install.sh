70d4a2124ce9b685e2b71cb3014775a518182ada
Rafael Gieschke <rafael.gieschke@rz.uni-freiburg.de>
2018-03-20 16:35:24 +0100

    Update packages, do not change Docker driver for bwCloud

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
+PROVIDER="$(cat /provider || true)"
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
 # docker installation
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
-sudo bash -c 'echo "{\"storage-driver\": \"aufs\"}" > /etc/docker/daemon.json'
+if ! test "$PROVIDER" = "bwcloud"; then
+  sudo bash -c 'echo "{\"storage-driver\": \"aufs\"}" > /etc/docker/daemon.json'
+fi
 sudo systemctl restart docker.service
 
 
