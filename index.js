f1a9221e9be7b276ebad15a8f6a8dda3115a914d
Rafael Gieschke <rafael@gieschke.de>
2019-06-20 13:30:42 +0200

    Add debug mode

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import WebSocket from "ws";
 
+const DEBUG = process.env.DEBUG === "1";
+if (DEBUG) console.debug("DEBUG mode enabled");
+if (!DEBUG) console.debug = () => {};
+
 const wss = new WebSocket.Server({
   port: 8080,
 });
 
 const channels = new Map();
+
+const channelInfo = channels => [...channels]
+  .map(([k, v]) => [k, [...v].map(v => 1)]);
 
 wss.on("connection", (ws, req) => {
   const url = new URL(req.url, "wss://invalid.invalid/");
@@ -15,6 +22,8 @@ wss.on("connection", (ws, req) => {
   channels.get(ch).add(ws);
 
   ws.on("message", m => {
+    if (DEBUG && ch === "/@@channels") m = JSON.stringify(channelInfo(channels));
+    console.debug("message", m);
     for (const c of channels.get(ch)) try {
       c.send(m);
     } catch {}
