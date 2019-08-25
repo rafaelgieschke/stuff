db645b3742fa331ae07d621a1860e0a57ca04da7
Rafael Gieschke <rafael@gieschke.de>
2019-08-25 12:36:35 +0200

    weakref_simple.h: Make linter happy

--- weakref_simple.h
+++ weakref_simple.h
@@ -8,10 +8,11 @@ struct ref {
 
 struct ref *get_ref(void *_Atomic *weakref) {
   uintptr_t _Atomic *_weakref = (uintptr_t _Atomic *)weakref, ref;
-  while ((ref = atomic_fetch_or(_weakref, 1)) & 1);
+  while ((ref = atomic_fetch_or(_weakref, 1)) & 1) (void)0;
   if (ref && ++((struct ref *)ref)->count == -1) *(volatile char *)0 = 0;
-  if (!atomic_compare_exchange_strong(_weakref, &(struct { uintptr_t _; })
-      {ref | 1}._, ref) && ref) ((struct ref *)ref)->count--;
+  uintptr_t locked_ref = ref | 1;
+  if (!atomic_compare_exchange_strong(_weakref, &locked_ref, ref))
+    ref && ((struct ref *)ref)->count--;
   return (struct ref *)ref;
 }
 
