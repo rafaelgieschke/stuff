947727e3ee97bbce800bc293dc3c13766e12f682
Rafael Gieschke <rafael@gieschke.de>
2019-10-15 01:10:24 +0200

    Try to send UDP packet to other server first

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ class StunSocket {
 const args = process.argv.slice(2);
 
 const once = (obj, type) => new Promise(r => obj.once(type, (...a) => r(a)));
+const timeout = time_ms => new Promise(r => setTimeout(r, time_ms));
 
 (async () => {
     const stunServers = args.length ? args : defaultServers;
@@ -44,7 +45,22 @@ const once = (obj, type) => new Promise(r => obj.once(type, (...a) => r(a)));
         console.log(`${address}:${port}`);
     }
     fetch(`${udpProxy}?${new URLSearchParams({ip: req[0].address, port: req[0].port})}`);
-    const [msg, rinfo] = await once(sock.socket, "message");
-    console.log(msg);
+    try {
+        const [msg, rinfo] = await Promise.race([once(sock.socket, "message"),
+             timeout(2000).then(() => {throw "Timeout"})]);
+        console.log(msg);
+    } catch (e) {
+        console.log(e);
+    }
 
+    sock.socket.send("+", 8080, new URL(udpProxy).hostname);
+    await timeout(1000);
+    fetch(`${udpProxy}?${new URLSearchParams({ip: req[0].address, port: req[0].port})}`);
+    try {
+        const [msg, rinfo] = await Promise.race([once(sock.socket, "message"),
+             timeout(2000).then(() => {throw "Timeout"})]);
+        console.log(msg);
+    } catch (e) {
+        console.log(e);
+    }
 })().catch(console.error).finally(() => process.exit());
